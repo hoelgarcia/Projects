d6b0aeb1bf089e511ac85fe73395e941235ff1bc
hoelgarcia <97121026+hoelgarcia@users.noreply.github.com>
2022-05-03 11:09:55 -0500

    Updated Bootstrap

--- JavaScript/MoodSic/frontend/src/views/Dashboard.js
+++ JavaScript/MoodSic/frontend/src/views/Dashboard.js
@@ -1,6 +1,7 @@
 import React, {useEffect,useState} from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import dateFormat from "dateformat";
 
 const Dashboard = (props) => {
     const CLIENT_ID = "90d2672395aa46a4922c1d5819680697"
@@ -34,7 +35,7 @@ const Dashboard = (props) => {
     const logout = () => {
         setToken("")
         window.localStorage.removeItem("token")
-        history.push("/");
+        history.push("");
     }
 
     const searchAlbums = async (e) =>{
@@ -52,14 +53,17 @@ const Dashboard = (props) => {
     }
     const renderAlbums = () => {
         return albums.map(album =>(
-            <div className="p-3 border bg-1" key={album.id}>
-                {album.images.length ? <img width={"200px"} src={album.images[0].url} alt=""/> : <div>No Image</div>}
-                <br />
-                <p><a href={album.uri}>View on Spotify</a></p>
+            <div className="p-3 border bg-1 d-flex row " key={album.id}>
+                <div className="col">
+                {album.images.length ? <img width={"225px"} src={album.images[0].url} alt=""/> : <div>No Image</div>}
+                </div>
+                <div className="col">
                 <p>Album Name: {album.name}</p>
                 <p>Artist: {album.artists[0].name}</p>
-                <p>Total Tracks On Album: {album.total_tracks}</p>
-                <p>Album Release Date: {album.release_date}</p>
+                <p>Total Songs: {album.total_tracks}</p>
+                <p>Album Release Date: {dateFormat(album.release_date, "mmmm dS, yyyy")}</p>
+                <p className="text-center"><a href={album.uri}>View {album.name} on Spotify</a></p>
+                </div>
             </div>
         ))
     }
@@ -80,13 +84,15 @@ const Dashboard = (props) => {
     const renderArtists = () => {
         return artists.map(artist =>(
             <div  className="p-3 border bg-1" key={artist.id}>
-                {artist.images.length ? <img width={"200px"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
-                <br />
-                <p><a href={artist.uri}>View on Spotify</a></p>
+                <div className="text-center">
+                {artist.images.length ? <img width={"250px"} src={artist.images[0].url} alt=""/> : <div>No Image Available</div>}
                 <p>{artist.name}</p>
                 <p>Popularity: {artist.popularity}/100</p> 
-                <p className="d-inline-block text-truncate" style={{maxWidth: "200px"}}>Genres: {artist.genres[0]}, {artist.genres[1]}, {artist.genres[2]}</p>
+                <p className="d-inline-block text-truncate" style={{maxWidth: "200px"}}>Genres: {artist.genres[0]}, {artist.genres[1]}</p>
+                <p><a href={artist.uri}>View {artist.name} on Spotify</a></p>
+                </div>
             </div>
+
         ))
     }
     const searchTracks = async (e) => {
@@ -102,13 +108,30 @@ const Dashboard = (props) => {
         })
         setTracks(data.tracks.items)
     }
+    // const msToMinutesAndSeconds = (ms) =>{
+    //     var minutes = Math.floor(ms/60000);
+    //     var seconds = ((ms/60000)/1000).toFixed(0);
+    //     return `${minutes}:${(seconds < 10 ? "0" : "")}${seconds}`;
+    // }
     const renderTracks = () => {
         return tracks.map(track =>(
             <div className="p-3 border bg-1" key={track.id}>
-                {track.album.images.length ? <img width={"200px"} src={track.album.images[0].url} alt=""/> : <div>No Image</div>}
-                <br />
-                <p>Song: {track.name}</p>
-                <p>Album: {track.album.name}</p>
+                <div className="row">
+                    <div className="col">
+                        {track.album.images.length ? <img width={"200px"} src={track.album.images[0].url} alt=""/> : <div>No Image</div>}
+                    </div>
+                    <div className="col">
+                        <p>Song: {track.name}</p>
+                        <p>Artist: {track.artists[0].name}</p>
+                        <p>Album: {track.album.name}</p>
+                        {/* <p>Duration: .msToMinutesAndSeconds({track.duration_ms})</p> */}
+                        <p>Explicit: {track.explicit == true ? <span>Yes</span> : <span>No</span>}</p>
+                        <p>Popularity: {track.popularity}/100</p>
+                    </div>
+                </div>
+                <div className="row mt-2">
+                <audio className="bg-1" controls autoplay><source src={track.preview_url} type="audio/mpeg"/></audio>
+                </div>
             </div>
         ))
     }
@@ -120,17 +143,18 @@ const Dashboard = (props) => {
 
     return (
         <div>
-            <nav className="navbar navbar-light bg-dark">
-            <form className="form-inline d-flex ">
-                <button className="btn btn-outline-danger justify-content-end" type="button" onClick={logout}>Log Out of Spotify</button>
-            </form>
+            <nav class="navbar navbar-light bg-dark justify-content-between">
+                <h1 class="font-weight-bold">MoodSic</h1>
+                <form class="form-inline">
+                    <button className="btn btn-outline-danger " type="button" onClick={logout}>Log Out of Spotify</button>
+                </form>
             </nav>
         <div className="row g-2 ">
             <div className="col-4 d-flex justify-content-center">
                 <div className="p-3 border bg-1">
                     {token ?
                     <form onSubmit={searchAlbums}>
-                        <input className="w-75 mr-sm-2" type="text" placeholder="Album Name" onChange={e => setSearchKeyAlbums(e.target.value)}/>
+                        <input className="w-100 mr-sm-2" type="text" placeholder="Album Name" onChange={e => setSearchKeyAlbums(e.target.value)}/>
                         <button className="btn btn-outline-success my-2 my-sm-0" type={"submit"}>Search Albums</button>
                     </form > 
                         : <p>In order to gain access to the Spotify API, you must first authorize/verify your Spotify account by logging in.</p>
@@ -145,7 +169,7 @@ const Dashboard = (props) => {
                 <div className="p-3 border bg-1 ">
                     {token ?
                     <form onSubmit={searchArtists}>
-                        <input className="w-75 mr-sm-2" type="text" placeholder="Artist Name" onChange={e => setSearchKeyArtists(e.target.value)}/>
+                        <input className="w-100 mr-sm-2" type="text" placeholder="Artist Name" onChange={e => setSearchKeyArtists(e.target.value)}/>
                         <button className="btn btn-outline-success my-2 my-sm-0" type={"submit"}>Search Artists</button>
                     </form>
                         : <p>In order to gain access to the Spotify API, you must first authorize/verify your Spotify account by logging in.</p>
@@ -160,8 +184,8 @@ const Dashboard = (props) => {
                 <div className="p-3 border bg-1">
                     {token ?
                     <form onSubmit={searchTracks}>
-                        <input className="w-75 mr-sm-2" type="text" placeholder="Song Name" onChange={e => setSearchKeyTracks(e.target.value)}/>
-                        <button className="btn btn-outline-success my-2 my-sm-0" type={"submit"}>Search Songs</button>
+                        <input className="w-100 mr-sm-2" type="text" placeholder="Song Name" onChange={e => setSearchKeyTracks(e.target.value)}/>
+                        <button className="btn btn-outline-success my-2 my-sm-0 " type={"submit"}>Search Songs</button>
                     </form>
                         : <p>In order to gain access to the Spotify API, you must first authorize/verify your Spotify account by logging in.</p>
                     }
